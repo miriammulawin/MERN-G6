ca8a27475f995868cb12cb448bf7083e6c02da10
Rose Bren Dungo <dungorosebren34@gmail.com>
2025-10-19 00:23:56 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-
 const API_URL = "http://localhost:5000/students";
-
 export const getStudents = () => axios.get(API_URL);
 export const addStudent = (student) => axios.post(API_URL, student);
 export const updateStudent = (id, student) =>
